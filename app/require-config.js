c2984325ed4ec8f593891bc5199fad4a93ac65b6
aestrada7 <vanhelsing44@gmail.com>
2015-07-23 15:09:29 -0500

    Using require to load libraries, will eventually be used for more daunting tasks

---
+++ app/require-config.js
@@ -0,0 +1,11 @@
+require.config({
+  paths: {
+    "jquery": "./vendor/jquery/jquery",
+    "modernizr": "./vendor/modernizr/modernizr",
+    "foundation": "./vendor/foundation/js/foundation",
+    "angular": "./vendor/angular/angular",
+    "angular-ui-router": "./vendor/angular-ui-router/angular-ui-router",
+    "indexeddb": "./vendor/angularjs-indexedDB/indexeddb",
+    "app": "./app"
+  }
+});
