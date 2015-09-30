e10b4e52902087b4ca456213534196e21cd1ba4a
Antonio Estrada <aestrada7@outlook.com>
2015-09-29 22:17:10 -0500

    Autocomplete directive finished

--- app/components/directives/autocomplete/autocomplete.js
+++ app/components/directives/autocomplete/autocomplete.js
@@ -11,7 +11,59 @@ app.directive('axAutocomplete',
       },
       templateUrl: 'components/directives/autocomplete/autocomplete.html',
       link: function(scope, element, attrs) {
+        var searchField = element.find('input');
+        var selectedFieldIndex = 0;
         scope.items = {};
+        scope.showAutocomplete = false;
+        selectedFieldIndex = 0;
+
+        element.on('keydown', function(event) {
+          if(event.keyCode === 38) { //up arrow
+            event.preventDefault();
+            if(selectedFieldIndex === 1) {
+              selectedFieldIndex = element.find('.entry').length;
+            } else {
+              selectedFieldIndex--;
+            }
+          }
+          if(event.keyCode === 40) { //down arrow
+            event.preventDefault();
+            if(selectedFieldIndex === element.find('.entry').length) {
+              selectedFieldIndex = 1;
+            } else {
+              selectedFieldIndex++;
+            }
+          }
+          if(event.keyCode === 13) { //enter
+            event.preventDefault();
+            element.find('.entry:nth-child(' + selectedFieldIndex + ')').click();
+            searchField.focus();
+            selectedFieldIndex = 0;
+          }
+          console.log(element.find('.entry:nth-child(' + selectedFieldIndex + ')'));
+          element.find('.entry:nth-child(' + selectedFieldIndex + ')').focus();
+        })
+
+        searchField.on('keyup', function(event) {
+          if(event.keyCode !== 13) { //enter
+            scope.showAutocomplete = searchField.val().length > 1;
+            scope.$apply();
+          }
+        });
+
+        element.on('blur', function() {
+          scope.showAutocomplete = false;
+          scope.$apply();
+        });
+
+        scope.textFilter = function(item) {
+          return item.key.search(searchField.val()) != -1;
+        }
+
+        scope.selectItem = function(item) {
+          searchField.val(item[scope.field]);
+          scope.showAutocomplete = false;
+        }
 
         fetchData({db: scope.db, field: scope.field}).then(function(results) {
           scope.items = results;
@@ -19,7 +71,7 @@ app.directive('axAutocomplete',
 
         function fetchData(args) {
           var defer = $q.defer();
-          if(args.db === "accounts") { // && args.field === "key"
+          if(args.db === "accounts") {
             accountsDB.find({}, function(err, results) {
               defer.resolve(results);
             });
