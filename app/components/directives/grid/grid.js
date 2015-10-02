d55e0703ae8127504ecda7c4315c9a06fc2109c4
Antonio Estrada <aestrada7@outlook.com>
2015-10-02 00:07:40 -0500

    Autocomplete and Grid now play together nicely

--- app/components/directives/grid/grid.js
+++ app/components/directives/grid/grid.js
@@ -3,28 +3,28 @@ app.directive('axGrid',
     return {
       restrict: 'A',
       link: function(scope, element, attrs) {
-        element.find('input').on('keyup', function(event) {
+        element.on('keyup', 'input', function(event) {
           var dispatcher = $(event.target);
           switch(event.keyCode) {
             case 37: //left
               var cursorLocation = dispatcher.val().slice(0, event.target.selectionStart).length;
               if(cursorLocation === 0) {
-                dispatcher.parent('.columns').prev('.columns').find('input').focus();
+                dispatcher.parents('.columns').prev('.columns').find('input').focus();
               }
               break;
             case 38: //up
-              var column = dispatcher.parent('.columns').index() + 1;
-              dispatcher.parent('.columns').parent('.row').prev('.row').find('.columns:nth-child(' + column + ')').find('input').focus();
+              var column = dispatcher.parents('.columns').index() + 1;
+              dispatcher.parents('.columns').parent('.row').prev('.row').find('.columns:nth-child(' + column + ')').find('input').focus();
               break;
             case 39: //right
               var cursorLocation = dispatcher.val().slice(0, event.target.selectionStart).length;
               if(cursorLocation === dispatcher.val().length) {
-                dispatcher.parent('.columns').next('.columns').find('input').focus();
+                dispatcher.parents('.columns').next('.columns').find('input').focus();
               }
               break;
             case 40: //down
-              var column = dispatcher.parent().index() + 1;
-              dispatcher.parent('.columns').parent('.row').next('.row').find('.columns:nth-child(' + column + ')').find('input').focus();
+              var column = dispatcher.parents('.columns').index() + 1;
+              dispatcher.parents('.columns').parent('.row').next('.row').find('.columns:nth-child(' + column + ')').find('input').focus();
               break;
           }
         });
