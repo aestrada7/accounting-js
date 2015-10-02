929e60610d8983685067ba0d888064bb3487e72e
Antonio Estrada <aestrada7@outlook.com>
2015-10-01 23:15:27 -0500

    Fixing changes in the grid to account for autocomplete cells

--- app/components/directives/grid/grid.js
+++ app/components/directives/grid/grid.js
@@ -9,22 +9,22 @@ app.directive('axGrid',
             case 37: //left
               var cursorLocation = dispatcher.val().slice(0, event.target.selectionStart).length;
               if(cursorLocation === 0) {
-                dispatcher.parent().prev().find('input').focus();
+                dispatcher.parent('.columns').prev('.columns').find('input').focus();
               }
               break;
             case 38: //up
-              var column = dispatcher.parent().index() + 1;
-              dispatcher.parent().parent().prev().find('div:nth-child(' + column + ') > input').focus();
+              var column = dispatcher.parent('.columns').index() + 1;
+              dispatcher.parent('.columns').parent('.row').prev('.row').find('.columns:nth-child(' + column + ')').find('input').focus();
               break;
             case 39: //right
               var cursorLocation = dispatcher.val().slice(0, event.target.selectionStart).length;
               if(cursorLocation === dispatcher.val().length) {
-                dispatcher.parent().next().find('input').focus();
+                dispatcher.parent('.columns').next('.columns').find('input').focus();
               }
               break;
             case 40: //down
               var column = dispatcher.parent().index() + 1;
-              dispatcher.parent().parent().next().find('div:nth-child(' + column + ') > input').focus();
+              dispatcher.parent('.columns').parent('.row').next('.row').find('.columns:nth-child(' + column + ')').find('input').focus();
               break;
           }
         });
