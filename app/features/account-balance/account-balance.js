07c6d7a16624f5be4592f8f5a71acffe4acd0c95
Antonio Estrada <aestrada7@outlook.com>
2015-10-23 14:04:36 -0500

    Added new report to dashboard, basic account search

--- app/features/account-balance/account-balance.js
+++ app/features/account-balance/account-balance.js
@@ -1,6 +1,46 @@
 app.controller('AccountBalanceController', 
-  ['$scope', '$q', 'notificationService',
+  ['$scope', '$q', 'notificationService', 'translateService',
 
-  function($scope, $q, notificationService) {
+  function($scope, $q, notificationService, translateService) {
+    $scope.accountBalance = {
+      accountName: '',
+      accountKey: ''
+    }
+
+    getAccountData = function(args) {
+      var defer = $q.defer();
+      accountsDB.find(args, function(err, results) {
+        if(err) {
+          defer.reject();
+        }
+        defer.resolve(results);
+      });
+      return defer.promise;
+    }
+
+    $scope.onChangeKey = function() {
+      getAccountData({key: $scope.accountBalance.accountKey}).then(function(results) {
+        if(results.length > 0) {
+          $scope.accountBalance.accountName = translateService.translate(results[0].name);
+        }
+      });
+    }
+
+    $scope.onChangeName = function() {
+      var itemName = '';
+      getAccountData().then(function(results) {
+        angular.forEach(results, function(value, key) {
+          if($scope.accountBalance.accountName === translateService.translate(results[key].name)) {
+            itemName = results[key].name;
+          }
+        });
+        if(itemName) {
+          getAccountData({name: itemName}).then(function(results) {
+            $scope.accountBalance.accountKey = results[0].key;
+          });
+        }
+      });
+    }
+
   }]
 );
