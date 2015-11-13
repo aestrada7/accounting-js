8066d828be83b40f715afbbb1555a98259447cef
Antonio Estrada <aestrada7@outlook.com>
2015-11-12 23:22:46 -0600

    Prototype for general balance, need to fetch data

--- app/features/general-balance/general-balance.js
+++ app/features/general-balance/general-balance.js
@@ -3,7 +3,23 @@ app.controller('GeneralBalanceController',
 
   function($scope, $q, $controller, notificationService, translateService) {
     $scope.generalBalance = {
+      floatingAssetsTotal: 900,
+      propertiesTotal: 0,
+      deferredAssetsTotal: 0,
+      shortTermTotal: 0,
+      longTermTotal: 0,
+      activeAssetsTotal: 900,
+      passiveAssetsTotal: 0,
+      stockholdersAccountsTotal: 0
     }
+    //mock data
+    $scope.floatingAssetsAccounts = [{
+      name: 'Account 1',
+      total: 200
+    }, {
+      name: 'Account 2',
+      total: 700
+    }];
 
     $('.loading').fadeOut(200);
   }]
